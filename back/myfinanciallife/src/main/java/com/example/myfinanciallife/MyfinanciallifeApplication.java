efdec7dea11baf7218708141949fea75c9640a65
Nicolas O <nicolasolaya27@gmail.com>
2026-03-02 15:59:07 -0500

    First commit

---
+++ back/myfinanciallife/src/main/java/com/example/myfinanciallife/MyfinanciallifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfinanciallife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinanciallifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinanciallifeApplication.class, args);
+	}
+
+}
