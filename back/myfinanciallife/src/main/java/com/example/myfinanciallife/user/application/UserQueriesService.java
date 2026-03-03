c4f0c5362e3e0849ccac575bcc5428e74cddbcae
Nicolas O <nicolasolaya27@gmail.com>
2026-03-02 20:38:28 -0500

    Drop unnecessary imports

--- back/myfinanciallife/src/main/java/com/example/myfinanciallife/user/application/UserQueriesService.java
+++ back/myfinanciallife/src/main/java/com/example/myfinanciallife/user/application/UserQueriesService.java
@@ -1,16 +1,12 @@
 package com.example.myfinanciallife.user.application;
 
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 import com.example.myfinanciallife.user.application.dto.UserResponse;
 import com.example.myfinanciallife.user.domain.User;
 import com.example.myfinanciallife.user.domain.UserRepository;
 import com.example.myfinanciallife.user.domain.exception.ApiException;
-import com.example.myfinanciallife.user.domain.exception.GlobalExceptionHandler;
-import com.example.myfinanciallife.user.domain.exception.UserNotFoundException;
+
 
 @Service
 public class UserQueriesService {
