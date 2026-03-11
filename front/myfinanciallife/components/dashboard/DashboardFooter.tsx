628b71a88bbc4081a7db0886f3ce3f607a285abb
Nicolas O <nicolasolaya27@gmail.com>
2026-03-10 19:35:01 -0500

    Light/Dark theme mini bug fixed

--- front/myfinanciallife/components/dashboard/DashboardFooter.tsx
+++ front/myfinanciallife/components/dashboard/DashboardFooter.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 export default function DashboardFooter() {
+
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const logo = theme === "dark"
+    ? "/logonordark.png"
+    : "/logonorlight.png";
 
   return (
     <footer className="mt-16 border-t p-6 flex justify-center items-center gap-3">
@@ -8,7 +26,7 @@ export default function DashboardFooter() {
       </span>
 
       <img
-        src="/logonor.png"
+        src={logo}
         alt="My brand"
         className="h-6"
       />
