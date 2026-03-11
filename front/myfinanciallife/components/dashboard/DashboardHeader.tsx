9b934e8bebfd90dc546d628b24e47f0a9f889462
Nicolas O <nicolasolaya27@gmail.com>
2026-03-11 09:56:41 -0500

    Modification on header, ready to continue with the next pages

--- front/myfinanciallife/components/dashboard/DashboardHeader.tsx
+++ front/myfinanciallife/components/dashboard/DashboardHeader.tsx
@@ -3,6 +3,7 @@
 import { User, LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import ThemeToggle from "@/components/ui/theme-toggle";
+import Link from "next/link";
 
 export default function DashboardHeader() {
 
@@ -15,23 +16,45 @@ export default function DashboardHeader() {
   return (
     <header className="flex justify-between items-center p-6 border-b">
 
-      {/* Left */}
-      <h1 className="text-2xl font-bold">
-        My Financial Life
-      </h1>
+      {/* Left side */}
+      <div className="flex items-center gap-8">
 
-      {/* Right */}
+        <h1 className="text-2xl font-bold">
+          My Financial Life
+        </h1>
+
+        <nav className="flex gap-6 text-sm">
+
+          <Link
+            href="/dashboard"
+            className="hover:text-primary"
+          >
+            Dashboard
+          </Link>
+
+          <Link
+            href="/financial-records"
+            className="hover:text-primary"
+          >
+            Financial Records
+          </Link>
+
+        </nav>
+
+      </div>
+
+      {/* Right side */}
       <div className="flex items-center gap-6">
 
         <ThemeToggle />
-        
+
         <div className="flex items-center gap-2 cursor-pointer">
-          <User size={20} />
+          <User size={20}/>
           <span>Profile</span>
         </div>
 
-        <button onClick={backToLogin} className="flex items-center gap-2 text-red-500 hover:text-red-400 cursor-pointer">
-          <LogOut size={18} />
+        <button className="flex items-center gap-2 text-red-500">
+          <LogOut size={18}/>
           Logout
         </button>
 
