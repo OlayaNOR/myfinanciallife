2a7e011b3c6273abd849264e62b06d442b6cedec
Nicolas O <nicolasolaya27@gmail.com>
2026-03-12 20:49:29 -0500

    Add the public url to cors config

--- back/myfinanciallife/src/main/java/com/example/myfinanciallife/security/config/SecurityConfig.java
+++ back/myfinanciallife/src/main/java/com/example/myfinanciallife/security/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://myfinanciallife.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT", "PATCH", "DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
